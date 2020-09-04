0b88c37e4dc421db40a50f8043aa33007b1fd6ea
arjaz <arjaz@protonmail.com>
2020-09-04 16:01:24 +0300

    feat: add some comments

--- main.py
+++ main.py
@@ -27,14 +27,24 @@ def bop_command(update, context):
 
 
 def main():
+    '''Start the bot'''
+    # Create the Updater and pass it your bot's token.
     updater = Updater('1265390639:AAHrUXDmTx-So3HJlM_s8ZL_zlcoFAiaErY',
                       use_context=True)
+
+    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
+    # Add commands handling
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('bop', bop_command))
 
+    # Start the Bot
     updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
